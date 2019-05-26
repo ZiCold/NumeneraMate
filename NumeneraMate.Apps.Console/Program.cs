e8b87d004ed4504a3eb81a1d8f5e6edb79246998
Igor Ponomarev <ia.ponomarev@hotmail.com>
2019-05-26 21:56:29 +0700

    Add ViewUniqueAttributes method to create classes

--- NumeneraMate.Apps.Console/Program.cs
+++ NumeneraMate.Apps.Console/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using NumeneraMate.Libs.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NumeneraMate.Apps.Console
 {
@@ -10,6 +12,32 @@ namespace NumeneraMate.Apps.Console
 	{
 		static void Main(string[] args)
 		{
+			var dir = @"E:\Documents\Tabletop RPGs\Numenera\APPs\XMLFilesFinal\";
+			var fileName = "Cyphers_Discovery.xml";
+			ViewUniqueAttributes(dir + fileName, "Cypher");
+		}
+
+		/// <summary>
+		/// View all unique attributes to create specific class
+		/// </summary>
+		/// <param name="xmlFile"></param>
+		/// <param name="element"></param>
+		private static void ViewUniqueAttributes(string xmlFile, string element)
+		{
+			XDocument doc = XDocument.Load(xmlFile);
+			var uniqueElements = new List<string>();
+			foreach(var elem in doc.Elements().First().Elements(element))
+			{
+				// elements() returns direct children
+				// descendants() recurses
+				foreach(var attr in elem.Elements())
+				{
+					var name = attr.Name.ToString();
+					if (!uniqueElements.Contains(name))
+						uniqueElements.Add(name);
+				}
+			}
+			uniqueElements.ForEach(x => System.Console.WriteLine(x + " "));
 		}
 	}
 }
