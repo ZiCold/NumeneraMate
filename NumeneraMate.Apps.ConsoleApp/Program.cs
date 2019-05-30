86df8db7376100c4f4a58110fc0d38842e9d27fb
Igor Ponomarev <ia.ponomarev@hotmail.com>
2019-05-30 21:05:36 +0700

    Refactor: Rename Console to ConsoleApp

--- NumeneraMate.Apps.ConsoleApp/Program.cs
+++ NumeneraMate.Apps.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace NumeneraMate.Apps.Console
+namespace NumeneraMate.Apps.ConsoleApp
 {
 	class Program
 	{
--- NumeneraMate.Apps.ConsoleApp/Properties/AssemblyInfo.cs
+++ NumeneraMate.Apps.ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NumeneraMate.Apps.Console")]
+[assembly: AssemblyTitle("NumeneraMate.Apps.ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NumeneraMate.Apps.Console")]
+[assembly: AssemblyProduct("NumeneraMate.Apps.ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
