4745ced6611cfea9a544e9b2ac280df2841e0dd9
Igor Ponomarev <misterzicold@gmail.com>
2020-09-01 19:52:21 +0700

    Add Device Dictionary Building - works

--- NumeneraMate.Support.Parsers/NumeneraDevicesParser.cs
+++ NumeneraMate.Support.Parsers/NumeneraDevicesParser.cs
@@ -35,9 +35,9 @@ namespace NumeneraMate.Support.Parsers
             switch (numeneraDeviceType)
             {
                 case DeviceType.Cypher:
-                    KeywordsList = new List<string>() { "Level:", "Internal:", "Wearable:", "Usable:", "Effect:", "#Table:" }; break;
+                    KeywordsList = new List<string>() { "Level:", "Internal:", "Wearable:", "Usable:", "Effect:", "Table:" }; break;
                 case DeviceType.Artefact:
-                    KeywordsList = new List<string>() { "Level:", "Form:", "Effect:", "#Table:", "Depletion:" }; break;
+                    KeywordsList = new List<string>() { "Level:", "Form:", "Effect:", "Table:", "Depletion:" }; break;
                 case DeviceType.Oddity:
                     KeywordsList = new List<string>(); break;
             }
@@ -49,8 +49,8 @@ namespace NumeneraMate.Support.Parsers
             var textLines = linesArray.Where(x => !string.IsNullOrEmpty(x)).ToList();
             textLines = TextFromPdfWordsFixer.ClearText(textLines);
 
-            // if in a single line more then one keyword - unnessecary step
-            //var keywordsLines = SplitLinesByKeywords(textLines);
+            // if in a single line more then one keyword
+            var keywordsLines = RemoveMoreThanOneKeyWordoccurrences(textLines);
 
             // Get List of cyphers dictionary List<Dictionary<keyword, string>>
             // cause next we can create XML, CSV, Excel table or return specific objects
@@ -59,32 +59,96 @@ namespace NumeneraMate.Support.Parsers
 
             // Get list of cyphers as List of objects
 
-            
-            Console.WriteLine();
+            foreach (var device in devicesAsDictionariesList)
+            {
+                foreach (var key in device.Keys)
+                {
+                    Console.WriteLine(key + " " + device[key]);
+                }
+                Console.WriteLine();
+            }
         }
 
         private List<Dictionary<string, string>> GetDevicesDictionaries(List<string> textLines)
         {
             var resultList = new List<Dictionary<string, string>>();
-            var currentDevice = new Dictionary<string, string>();
             var linesArray = textLines.ToArray();
             for (int i = 0; i < linesArray.Length; i++)
             {
-                currentDevice.Add(NameKeyword, linesArray[i]);
-                i = BuildCurrentDevice(ref linesArray, i, currentDevice);
+                i = BuildCurrentDevice(linesArray, i, out var currentDevice);
 
-                resultList.Add(currentDevice);
-                currentDevice.Clear();
+                var nextObj = new Dictionary<string, string>();
+                foreach (var key in currentDevice.Keys)
+                    nextObj.Add(key, currentDevice[key].Trim());
+                resultList.Add(nextObj);
             }
             return resultList;
         }
 
-        private int BuildCurrentDevice(ref string[] linesArray, int i, Dictionary<string, string> currentDevice)
+        private int BuildCurrentDevice(string[] lines, int currentIndex, out Dictionary<string, string> curObj)
         {
-            throw new NotImplementedException();
+            curObj = new Dictionary<string, string>();
+            curObj.Add(NameKeyword, lines[currentIndex]);
+            // read current object body starting from the next string
+            var currentKeyword = "";
+            int j;
+            for (j = currentIndex + 1; j < lines.Length; j++)
+            {
+                if (string.IsNullOrEmpty(lines[j])) continue;
+                // либо конец массива строк, либо начало следующего объекта
+                if (j + 1 != lines.Length && lines[j + 1].StartsWith(KeywordsList.First())) 
+                    return j - 1;
+
+                // check line for Keyword and add new line
+                foreach (var keyword in KeywordsList)
+                    if (lines[j].StartsWith(keyword))
+                    {
+                        currentKeyword = keyword;
+                        break;
+                    }
+                if (currentKeyword != "")
+                {
+                    if (curObj.ContainsKey(currentKeyword))
+                    {
+                        curObj[currentKeyword] += " " + lines[j];
+                    }
+                    else
+                    {
+                        var startOfDescription = lines[j].Substring(currentKeyword.Length);
+                        curObj.Add(currentKeyword, startOfDescription);
+                    }
+                    continue;
+                }
+                else
+                {
+                    curObj[NameKeyword] += " " + lines[j];
+                    continue;
+                }
+
+                //if (lines[j + 1].StartsWith(KeywordsList.First())) return j - 1;
+
+
+                // if it's table, than build it OR add normal keyword/line
+                //if (info.TableKeyword != null && lines[j].Contains(info.TableKeyword))
+                //{
+                //    curObj[curObj.Count - 1] += lines[j] + " ";
+                //    j = BuildTable(lines, j, curObj, info.KeywordsList);
+                //}
+                //else
+                //{
+                //    curObj[curObj.Count - 1] += lines[j] + " ";
+                //}
+
+            }
+            return j;
         }
 
-        public List<string> SplitLinesByKeywords(List<string> itemsLines)
+        /// <summary>
+        /// Additional feature - split line if it has more than one keyword
+        /// </summary>
+        /// <param name="itemsLines"></param>
+        /// <returns></returns>
+        public List<string> RemoveMoreThanOneKeyWordoccurrences(List<string> itemsLines)
         {
             var result = new List<string>();
             foreach (var line in itemsLines)
@@ -94,10 +158,9 @@ namespace NumeneraMate.Support.Parsers
 
                 var wholeLineLength = curLine.Length;
                 var curLineLength = wholeLineLength;
-                var keyWordPos = curLineLength;
                 do
                 {
-                    keyWordPos = FindFirstKeyWordInLine(curLine, KeywordsList);
+                    int keyWordPos = FindFirstKeyWordInLine(curLine, KeywordsList);
                     if (keyWordPos < curLineLength)
                     {
                         var firstPart = curLine.Substring(0, keyWordPos);
