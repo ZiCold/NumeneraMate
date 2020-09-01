63bb45531fb7b3aa0c4088082173e2b85b7cb1f8
Igor Ponomarev <misterzicold@gmail.com>
2020-09-02 00:08:46 +0700

    Add NumeneraDevice base class and Artefact class

--- NumeneraMate.Libs.Devices/Cypher.cs
+++ NumeneraMate.Libs.Devices/Cypher.cs
@@ -5,55 +5,11 @@ using System.Xml.Serialization;
 namespace NumeneraMate.Libs.Devices
 {
     [Serializable, XmlRoot("Cypher")]
-    public class Cypher
+    public class Cypher : NumeneraDevice
     {
-        private int minimumCraftingLevel;
-
-        public string Name { get; set; }
-        public string Level { get; set; }
-        public int MinimumCraftingLevel
-        {
-            get
-            {
-                if (minimumCraftingLevel == 0 && !string.IsNullOrEmpty(Level))
-                {
-                    if (Level.Contains("+"))
-                    {
-                        minimumCraftingLevel = int.Parse(Level.Substring(Level.IndexOf("+")+1));
-                    }
-                    else
-                    {
-                        minimumCraftingLevel = 1;
-                    }
-                }
-                return minimumCraftingLevel;
-            }
-            set => minimumCraftingLevel = value;
-        }
-
         public string Wearable { get; set; }
         public string Usable { get; set; }
         public string Internal { get; set; }
-
-        public string Effect { get; set; }
-
-        public RollTable RollTable { get; set; }
-        [XmlIgnore]
-        public string TableAsString { get; set; }
-
-        public string Source { get; set; }
-
-        public override string ToString()
-        {
-            string result = "";
-            foreach (var p in this.GetType().GetProperties())
-            {
-                var name = p.Name;
-                var value = p.GetValue(this, null);
-                if (value != null) result += $"{name}: {value.ToString()}\n";
-            }
-            return result;
-        }
     }
 
 }
---
+++ NumeneraMate.Libs.Devices/NumeneraDevice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NumeneraMate.Libs.Devices
+{
+    public class NumeneraDevice
+    {
+        private int minimumCraftingLevel;
+
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public int MinimumCraftingLevel
+        {
+            get
+            {
+                if (minimumCraftingLevel == 0 && !string.IsNullOrEmpty(Level))
+                {
+                    if (Level.Contains("+"))
+                    {
+                        minimumCraftingLevel = int.Parse(Level.Substring(Level.IndexOf("+") + 1));
+                    }
+                    else
+                    {
+                        minimumCraftingLevel = 1;
+                    }
+                }
+                return minimumCraftingLevel;
+            }
+            set => minimumCraftingLevel = value;
+        }
+
+        public string Effect { get; set; }
+
+        public RollTable RollTable { get; set; }
+        [XmlIgnore]
+        public string TableAsString { get; set; }
+
+        public string Source { get; set; }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var p in this.GetType().GetProperties())
+            {
+                var name = p.Name;
+                var value = p.GetValue(this, null);
+                if (value != null) result += $"{name}: {value.ToString()}\n";
+            }
+            return result;
+        }
+    }
+}
