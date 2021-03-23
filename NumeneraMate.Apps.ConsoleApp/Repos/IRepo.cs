caf04384b91e52ac1c4e5f39320339dae1292553
Igor Ponomarev <misterzicold@gmail.com>
2021-03-23 17:48:41 +0700

    IRepo & XMLCYpherRepo in ConsoleApp

---
+++ NumeneraMate.Apps.ConsoleApp/Repos/IRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeneraMate.Apps.ConsoleApp
+{
+    // Interface for loading data from different locations: XML, JSON, Excel, SQLLite
+    // and for saving data (current devices list for example) to different locations: XML, JSON, Excel, SQLite
+    // IRepo<Cypher> -> XMLRepoBase<Cypher> -> CypherRepo
+    public interface IRepo<T>
+    {
+        bool IsChangesProhibited { get; }
+        List<T> GetAll();
+        T GetOne(string name);
+        void Add(T Entity);
+        void AddRange(T Entity);
+        void Delete(string name);
+        void Save();
+    }
+}
---
+++ NumeneraMate.Apps.ConsoleApp/Repos/XMLCypherRepo.cs
@@ -0,0 +1,52 @@
+﻿using NumeneraMate.Libs.Devices;
+using NumeneraMate.Support.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeneraMate.Apps.ConsoleApp
+{
+    public class XMLCypherRepo : IRepo<Cypher>
+    {
+        public bool IsChangesProhibited { get; }
+        string _xmlFileName;
+
+        public XMLCypherRepo(bool isChangesProhibited, string xmlFileName)
+        {
+            IsChangesProhibited = isChangesProhibited;
+            _xmlFileName = xmlFileName;
+        }
+
+        public List<Cypher> GetAll()
+        {
+            return NumeneraXML.DeserializeCyphersListFromXML(_xmlFileName);
+        }
+
+        public Cypher GetOne(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Cypher Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(Cypher Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
