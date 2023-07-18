46933b9d5a975b40c895f034662adc9d9d6d671d
Igor Ponomarev <misterzicold@gmail.com>
2023-07-18 17:56:05 +0700

    add NumeneraMate.Apps.ConsoleAppDotNet net6.0

---
+++ NumeneraMate.Apps.ConsoleAppDotNet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NumeneraMate.Apps.ConsoleAppDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
