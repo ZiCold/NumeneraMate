98d4f3117d50edbd08366b7675e13d77f792b7d3
Igor Ponomarev <misterzicold@gmail.com>
2023-07-19 20:39:15 +0700

    add creatures and encounters

---
+++ NumeneraMate.Libs.NumeneraObjects/Events/Encounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeneraMate.Libs.NumeneraObjects.Events
+{
+    public class Encounter
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string PlainsHills { get; set; }
+        public string Desert { get; set; }
+        public string Woods { get; set; }
+        public string Mountains { get; set; }
+        public string Swamp { get; set; }
+        public string Camp { get; set; }
+    }
+}
